84e3a236f89ab519c4060799ea18475b21cbe182
Jin Xu <xujincdl@cn.ibm.com>
2020-06-18 01:07:49 -0700

    Update to support Python 3

--- src/STATS_VALLBLS_FROMDATA.py
+++ src/STATS_VALLBLS_FROMDATA.py
@@ -3,13 +3,12 @@
 # *
 # * IBM SPSS Products: Statistics Common
 # *
-# * (C) Copyright IBM Corp. 2014
+# * (C) Copyright IBM Corp. 1989, 2020
 # *
 # * US Government Users Restricted Rights - Use, duplication or disclosure
-# * restricted by GSA ADP Schedule Contract with IBM Corp. 
+# * restricted by GSA ADP Schedule Contract with IBM Corp.
 # ************************************************************************/
 
-from __future__ import with_statement
 import random, os, tempfile, textwrap, codecs, re, locale
 import spss, spssaux, spssdata, textwrap
 
@@ -291,7 +290,7 @@ AGGREGATE /OUTFILE=%s /BREAK %s"""
         # write report
         
         if not labelsyntax:
-            print _("""No value labels were generated.""")
+            print(_("""No value labels were generated."""))
             return
         
         if len(self.labelvars) > 1:
@@ -471,7 +470,7 @@ class NonProcPivotTable(object):
 def attributesFromDict(d):
     """build self attributes from a dictionary d."""
     self = d.pop('self')
-    for name, value in d.iteritems():
+    for name, value in d.items():
         setattr(self, name, value)
 
 def StartProcedure(procname, omsid):
@@ -553,7 +552,7 @@ class FileHandles(object):
 def Run(args):
     """Execute the STATS VALLBLS FROMDATA extension command"""
 
-    args = args[args.keys()[0]]
+    args = args[list(args.keys())[0]]
 
     oobj = Syntax([
         Template("VARIABLES", subc="",  ktype="varname", var="variables", islist=True),
@@ -576,7 +575,7 @@ def Run(args):
         def _(msg):
             return msg
     # A HELP subcommand overrides all else
-    if args.has_key("HELP"):
+    if "HELP" in args:
         #print helptext
         helper()
     else:
@@ -596,7 +595,7 @@ def helper():
     # webbrowser.open seems not to work well
     browser = webbrowser.get()
     if not browser.open_new(helpspec):
-        print("Help file not found:" + helpspec)
+        print(("Help file not found:" + helpspec))
 try:    #override
     from extension import helper
 except:
